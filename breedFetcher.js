269b7a3d229989e02bca23c2f1888930bb8dd508
mckennaleo <mckenna.leo@gmail.com>
2020-07-02 20:31:55 +0000

    catz

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,17 @@
-const request =  require("request");
-const args = process.argv.slice(2);
-const race =  args[0].slice(0, 4).toLowerCase();
-request.get(`https://api.thecatapi.com/v1/images/search?breed_id=${race}`, function(error, response, body) {
-  if (error !== null) {
-    console.log('Error: Request failed.');
-  }
-  const data = JSON.parse(body);
-  if (data[0] === undefined) {
-    console.log('Error: Breed not found.');
-  } else {
-    console.log(data[0]);
-  }
-});
+const request =  require('request');
+const fetchBreedDescription = (breedName, callback) => {
+  request.get(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(err, response, body) {
+    if (err !== null) {
+      callback(err, null);
+    }
+    const data = JSON.parse(body);
+    if (data[0] === undefined) {
+      callback(null, 'Breed not found');
+    } else {
+      callback(null, data[0].description);
+    }
+  });
+
+};
+
+module.exports = { fetchBreedDescription };
